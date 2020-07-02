948e82810b8e8f675cc435e018dd3bdaf0c196fb
akhzan <rizal@payfazz.com>
2020-07-02 12:09:47 +0700

    new web

---
+++ precache-manifest.1f9038a10c213144f5ecdb8bb3aacfad.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba971a943e1f89b5d0525fc46dd78539",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01524dedc1913c1ca873",
+    "url": "/static/css/2.b9d9fd7e.chunk.css"
+  },
+  {
+    "revision": "136e663d87239f5729dc",
+    "url": "/static/css/main.92eb8cf3.chunk.css"
+  },
+  {
+    "revision": "01524dedc1913c1ca873",
+    "url": "/static/js/2.3d000113.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3d000113.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "136e663d87239f5729dc",
+    "url": "/static/js/main.20f497cf.chunk.js"
+  },
+  {
+    "revision": "17539a5d08637f3cb0eb",
+    "url": "/static/js/runtime-main.d71dc062.js"
+  },
+  {
+    "revision": "9de38137491e530bf1ac875e206e4229",
+    "url": "/static/media/b.9de38137.jpg"
+  },
+  {
+    "revision": "d45bfccac81fb05c63c7c1fb59d59c2f",
+    "url": "/static/media/c.d45bfcca.jpg"
+  },
+  {
+    "revision": "3d7041c8a7fe9aba1a3b56447b6b320c",
+    "url": "/static/media/canfazzdashboard.3d7041c8.png"
+  },
+  {
+    "revision": "3dcf20dddca13956b1e85bf581939bf9",
+    "url": "/static/media/canfazzweb.3dcf20dd.png"
+  },
+  {
+    "revision": "1c244777afffb1d37710a80fc5431b36",
+    "url": "/static/media/d.1c244777.jpg"
+  },
+  {
+    "revision": "e2128db4d04b86bd6c56fa8a856e809a",
+    "url": "/static/media/e.e2128db4.jpg"
+  },
+  {
+    "revision": "5ada66230eb2f306cf64e15d373ff75c",
+    "url": "/static/media/humaaans.5ada6623.png"
+  },
+  {
+    "revision": "31c017085211d0dd76f0363c06d7f78b",
+    "url": "/static/media/logo.31c01708.png"
+  },
+  {
+    "revision": "8522d740e1f2b2897bafb794f303eeba",
+    "url": "/static/media/porse.8522d740.png"
+  },
+  {
+    "revision": "053b17ead3729f78100d6e9b51322c50",
+    "url": "/static/media/sam.053b17ea.png"
+  }
+]);
